2afabdb46cfc5f12a1ff7c57d977e5f36be63b62
jwkim <phoongdeng@gmail.com>
2021-02-19 22:48:41 +0900

    Add django project

---
+++ config/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("snippets/", include("snippets.urls", namespace="snippets")),
+    path("admin/", admin.site.urls),
+]
